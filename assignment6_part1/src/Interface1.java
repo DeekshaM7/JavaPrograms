5d138ac6c13e42f6e826438f8041bef6483a2c17
DeekshaM7 <147788575+DeekshaM7@users.noreply.github.com>
2024-03-01 00:33:58 +0530

    Add files via upload

---
+++ assignment6_part1/src/Interface1.java
@@ -0,0 +1,7 @@
+package assignment6_part1;
+
+public  interface Interface1 {
+    int a=2;
+    public void method1();
+    public void method2();
+}
---
+++ assignment6_part1/src/Interface_STK.java
@@ -0,0 +1,9 @@
+package assignment6_part1;
+
+public interface Interface_STK {
+    public void push(int element);
+    public int pop();
+    public void displayStack();
+    public boolean isOverflow();
+    public boolean isUnderflow();
+}
