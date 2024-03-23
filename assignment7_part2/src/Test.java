0fd7a48545d02f92059ba4b5bb0f830599f62453
DeekshaM7 <147788575+DeekshaM7@users.noreply.github.com>
2024-03-23 20:19:23 +0530

    Add files via upload

---
+++ assignment7_part2/src/Test.java
@@ -0,0 +1,39 @@
+package Part2;
+
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.print("Enter a number to find its factorial (or type 'exit' to quit): ");
+                String input = scanner.nextLine();
+
+                if (input.equalsIgnoreCase("exit")) {
+                    break;
+                }
+
+                int num = Integer.parseInt(input);
+                if (num < 0 || num > 15) {
+                    throw new Part2.FactorialException(num);
+                }
+                System.out.println("Factorial of " + num + " is: " + factorial(num));
+            } catch (NumberFormatException e) {
+                System.out.println("NumberFormatException: Invalid input format.");
+            } catch (Part2.FactorialException e) {
+                System.out.println(e);
+            }
+        }
+        scanner.close();
+    }
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1)
+            return 1;
+        else
+            return n * factorial(n - 1);
+    }
+
+}
